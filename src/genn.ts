09aed9ac1c3b33755047d65f3db62ec8e2697605
julian <jh@practio.com>
2019-07-08 16:17:52 +0200

    first

---
+++ src/genn.ts
@@ -0,0 +1,74 @@
+import * as program from 'commander'
+import { ensureDir, readFile, writeFile } from 'fs-extra'
+import {join, resolve} from 'path'
+
+program
+  .option('-c, --component <compName>', 'add component')
+  .option('-o, --container <containerName>', 'add container')
+  .option('-n, --dry', 'Test run, no side effects')
+  .parse(process.argv)
+
+const dry = (program.dry !== undefined)
+
+if (program.component) {
+  componentCmd(program.component)
+}
+else if(program.container) {
+  containerCmd(program.container)
+}
+
+async function containerCmd (newName:string) {
+  const compName = newName.replace('Home','').replace('Container','')
+  const containerCompName = compName + "Container"
+
+  const tpl = (await readFile(join(__dirname, '../src/tpl', 'container.tpl.jsx')))
+    .toString()
+    .replace(new RegExp('CONTAINER_NAME','g'), containerCompName)
+    .replace(new RegExp('COMPONENT_NAME','g'), compName)
+
+  const cwDir = resolve(process.cwd(), '.')
+  console.log('cwd (should be components)', cwDir)
+
+  // make its parent dir
+  if (!dry)
+    await ensureDir(join(cwDir, compName))
+  else
+    console.log("make dir ",join(cwDir, compName))
+
+  // write the file
+  const newFilePath = join(compName, containerCompName + '.js')
+  if (!dry)
+    await writeFile(newFilePath , tpl)
+  else
+    console.log("write file ", newFilePath, tpl.toString().length + " B")
+
+  console.log("Done")
+}
+
+async function componentCmd (newName:string) {
+  console.log('create', newName)
+  const tpl = (await readFile(join(__dirname, '../src/tpl', 'component.tpl.jsx')))
+    .toString()
+    .replace(new RegExp('COMPONENT_NAME','g'), newName)
+
+  const cwDir = resolve(process.cwd(), '.')
+  console.log('cwd (should be components)', cwDir)
+
+  // make its parent dir
+  const newDir = newName.replace('Home','').replace('Container','')
+
+  if (!dry)
+    await ensureDir(join(cwDir, newDir))
+  else
+    console.log("make dir ",join(cwDir, newDir))
+
+  // write the file
+  const newFilePath = join(newDir, newName + '.js')
+  if (!dry)
+    await writeFile(newFilePath, tpl)
+  else
+    console.log("write file ", newFilePath, tpl.toString().length + " B")
+
+  console.log("Done")
+}
+
---
+++ src/tpl/container.tpl.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { PropTypes } from 'prop-types'
+import COMPONENT_NAME from './COMPONENT_NAME'
+
+function CONTAINER_NAME ({ }) {
+
+  return (
+    <COMPONENT_NAME
+
+    />
+  )
+}
+
+CONTAINER_NAME.propTypes = {
+}
+
+export default connect(state => ({
+}))(CONTAINER_NAME)
